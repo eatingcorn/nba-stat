31be809a1a50975d1cc9ae5d38795b8244f68435
Ricky <ricky.fung@mail.utoronto.ca>
2024-03-26 22:43:53 +0000

    Implemented simulate data script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,24 +1,17 @@
 #### Preamble ####
 # Purpose: Simulates anaylsis dataset about Pokemon level
 # and weaknesses against fire
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Author: Ricky Fung
+# Date: 26 March 2024
+# Contact: ricky.fung@mail.utoronto.ca
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
 data <- 
   tibble(
     level = rnorm(n=1000, mean = 100, sd = 10) %>% floor(),
     
-    weakness = sample(x=c("Fire", "Not Fire"), size = 100, replace = TRUE)
+    weakness = sample(x=c("Fire", "Not Fire"), size = 1000, replace = TRUE)
   )
-
-
-
